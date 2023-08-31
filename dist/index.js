c234b0131dacbfc2b12cba4f32477f5d58540705
LucasMoskun <contact@lucasmoskun.com>
2023-08-31 19:14:15 +0100

    fix index.js namings

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,8 @@
 // index.js
-const DLinkedList = require("./d-link-list");
-const DLinkedNode = require("./d-link-node");
+const { DLinkList } = require("./d-link-list");
+const { DLinkNode } = require("./d-link-node");
 
 module.exports = {
-  DLinkedList,
-  DLinkedNode,
+  DLinkList,
+  DLinkNode,
 };
